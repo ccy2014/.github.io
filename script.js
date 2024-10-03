db84f92d723a802bc6dcd44e7a126c46d955f9a6
ccy2014 <yuanchen035@gmail.com>
2024-10-03 17:44:04 +1000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleLanguage() {
-    const cnElements = document.querySelectorAll('.cn');
-    const enElements = document.querySelectorAll('.en');
-    
-    cnElements.forEach(el => el.style.display = el.style.display === 'none' ? 'inline' : 'none');
-    enElements.forEach(el => el.style.display = el.style.display === 'none' ? 'inline' : 'none');
-}
